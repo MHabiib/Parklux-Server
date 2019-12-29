154588a8248cfe010917174cb6e4747406963d3c
MHabib <mhabibofficial@yahoo.com>
2019-12-30 06:20:25 +0700

    #45 update open hours

--- src/main/java/com/future/pms/Constants.java
+++ src/main/java/com/future/pms/Constants.java
@@ -52,7 +52,7 @@ public class Constants {
 
     public static final String CUSTOMER = "CUSTOMER";
     public static final String ADMIN = "ADMIN";
-    public static final String OPEN_HOUR = "-- : -- / -- : --";
+    public static final String OPEN_HOUR = "00:00 - 00:00";
     public static final String FILE_LOCATION = "../qr/";
     public static final String SLOT_UPDATED = "Slot Updated";
     public static final String PARKING_ZONE_NOT_FOUND = "Parking zone not found !";
